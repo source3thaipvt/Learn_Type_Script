db530e2563fbd2793d599ba1a84a54a9e5c2c25b
soucre.thaipvt <soucre.thaipvt@gmail.com>
2022-02-18 01:01:48 +0700

    Lab15: Duck typing: ko gan kieu van push dc vao array Contact

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import * as Lab9 from './lab9_optional_chaining';
 import * as Lab10 from './lab10_extend_interface';
 import * as Lab11 from './lab11_implement_interface';
 import * as Lab12 from './lab12_interface_declaration_merging';
+import * as Lab13 from './lab13_type_inference';
+import * as Lab14 from './lab14_contextual_typing';
 
 function sum(...nums) {
   return nums.reduce((a, b) => a + b);
@@ -21,3 +23,5 @@ Lab9;
 Lab10;
 Lab11;
 Lab12;
+Lab13;
+Lab14;
